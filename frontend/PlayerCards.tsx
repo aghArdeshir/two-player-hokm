7c7c0cf1365d75d9f27e4a93347e195aeb20c9ab
aghArdeshir <ardeshireo@gmail.com>
2021-01-09 00:15:41 +0330

    sort player's hands

--- frontend/PlayerCards.tsx
+++ frontend/PlayerCards.tsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from "react";
 import { GameStateContext } from "./GameStateContext";
-import { GAME_ACTION, ICard } from "../common.typings";
+import { CARD_FORMAT, GAME_ACTION, ICard } from "../common.typings";
 import Card from "./Card";
 import { isEqual } from "lodash";
 import { socketService } from "./socket-service";
@@ -10,6 +10,13 @@ function isValidToDrop(cards: ICard[]): cards is [ICard, ICard] {
   if (cards.length === 2) return true;
   return false;
 }
+
+const CARD_FORMAT_SUIT_ORDER = [
+  CARD_FORMAT.PIKES,
+  CARD_FORMAT.HEARTS,
+  CARD_FORMAT.CLOVERS,
+  CARD_FORMAT.TILES,
+];
 
 export default function PlayerCards() {
   const gameState = useContext(GameStateContext);
@@ -31,42 +38,67 @@ export default function PlayerCards() {
     <>
       These are your cards:
       <br />
-      {cardsToList.map((card: ICard, index: number) => (
-        <Card
-          key={card.format + card.number}
-          card={card}
-          style={{
-            transform: isChosen(card) ? "translateY(60px)" : "unset",
-            transition: "all 0.4s",
-            zIndex: index + 1,
-            position: "relative",
-          }}
-          onClick={(card) => {
-            if (gameState.nextAction === GAME_ACTION.DROP_TWO && !dropped) {
-              if (isChosen(card)) {
-                setCardsToDrop((ctd) => ctd.filter((c) => !isEqual(c, card)));
-              } else {
-                if (cardsToDrop.length === 2) {
-                  setCardsToDrop((ctd) => ctd.slice(1));
-                }
-                setCardsToDrop((ctd) => ctd.concat(card));
-              }
-            }
-
+      {cardsToList
+        .sort((cardA, cardB) => {
+          if (cardA.format === cardB.format) {
+            if(cardA.number === 1) return -1;
+            if(cardB.number === 1) return 1;
+            return cardA.number > cardB.number ? -1 : 1;
+          } else if (cardA.format === gameState.hokm) {
+            return -1;
+          } else if (cardB.format === gameState.hokm) {
+            return 1;
+          } else {
             if (
-              gameState.nextAction === GAME_ACTION.PLAY &&
-              gameState.player.isTurn &&
-              (card.format === gameState.cardOnGround?.format ||
-                !gameState.player.cards.find(
-                  (c) => c.format === gameState.cardOnGround?.format
-                ) ||
-                !gameState.cardOnGround)
+              CARD_FORMAT_SUIT_ORDER.indexOf(cardA.format) <
+              CARD_FORMAT_SUIT_ORDER.indexOf(cardB.format)
             ) {
-              socketService.play(card);
+              return -1;
             }
-          }}
-        />
-      ))}
+            if (
+              CARD_FORMAT_SUIT_ORDER.indexOf(cardB.format) <
+              CARD_FORMAT_SUIT_ORDER.indexOf(cardA.format)
+            ) {
+              return 1;
+            }
+          }
+        })
+        .map((card: ICard, index: number) => (
+          <Card
+            key={card.format + card.number}
+            card={card}
+            style={{
+              transform: isChosen(card) ? "translateY(60px)" : "unset",
+              transition: "all 0.4s",
+              zIndex: index + 1,
+              position: "relative",
+            }}
+            onClick={(card) => {
+              if (gameState.nextAction === GAME_ACTION.DROP_TWO && !dropped) {
+                if (isChosen(card)) {
+                  setCardsToDrop((ctd) => ctd.filter((c) => !isEqual(c, card)));
+                } else {
+                  if (cardsToDrop.length === 2) {
+                    setCardsToDrop((ctd) => ctd.slice(1));
+                  }
+                  setCardsToDrop((ctd) => ctd.concat(card));
+                }
+              }
+
+              if (
+                gameState.nextAction === GAME_ACTION.PLAY &&
+                gameState.player.isTurn &&
+                (card.format === gameState.cardOnGround?.format ||
+                  !gameState.player.cards.find(
+                    (c) => c.format === gameState.cardOnGround?.format
+                  ) ||
+                  !gameState.cardOnGround)
+              ) {
+                socketService.play(card);
+              }
+            }}
+          />
+        ))}
       <br />
       {gameState.nextAction === GAME_ACTION.DROP_TWO ? (
         <>
