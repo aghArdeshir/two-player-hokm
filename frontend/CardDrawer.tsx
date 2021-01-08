476d1b60f07b54b76eab3f936c71c540fc70e3f6
aghArdeshir <ardeshireo@gmail.com>
2021-01-08 15:41:27 +0330

    add a beautiful card drawer

---
+++ frontend/CardDrawer.tsx
@@ -0,0 +1,25 @@
+import { CARD_FORMAT, ICard } from "../common.typings";
+//@ts-ignore
+import deckAsPng from "./deck.png";
+
+export default function CardDrawer(props: { card: ICard }) {
+  let height = -9;
+  if (props.card.format === CARD_FORMAT.HEARTS) height = -204;
+  if (props.card.format === CARD_FORMAT.CLOVERS) height = -595;
+  if (props.card.format === CARD_FORMAT.TILES) height = -399;
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${deckAsPng})`,
+        width: 126,
+        height: 188,
+        backgroundSize: "1387%",
+        margin: 1,
+        display: "inline-block",
+        backgroundPositionX: -7 + (props.card.number - 1) * -134,
+        backgroundPositionY: height,
+      }}
+    />
+  );
+}
