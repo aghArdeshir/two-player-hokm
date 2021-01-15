d06f9765d4cfae112e8980731e3de6affbbaa0fb
aghArdeshir <ardeshireo@gmail.com>
2021-01-15 23:08:56 +0330

    minor styling improvement

--- frontend/OtherPlayerCards.tsx
+++ frontend/OtherPlayerCards.tsx
@@ -7,20 +7,36 @@ export default function OtherPlayerCards() {
   const gameState = useContext(GameStateContext);
 
   return (
-    <>
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        display: "flex",
+        left: "50%",
+        transform: "translateX(-50%)",
+      }}
+    >
       {Array(gameState.otherPlayer.cardsLength)
         .fill(1)
         .map((_, index: number) => (
-          <div style={{ display: "inline-block", width: 40 }} key={index}>
+          <div
+            key={index}
+            style={{
+              width: 20,
+              boxShadow: "0 0 10px black",
+            }}
+          >
             <div
+              className="opponent-card"
               style={{
-                width: 152,
-                height: 229,
+                width: 60,
+                height: 90,
+                backgroundSize: 60,
                 backgroundImage: `url(${cardBackAsPng})`,
               }}
             />
           </div>
         ))}
-    </>
+    </div>
   );
 }
