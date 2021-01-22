6c581376b38e2ebc69b2efa90c907bc609c901bc
aghArdeshir <ardeshireo@gmail.com>
2021-01-23 01:36:47 +0330

    layout improvement

--- frontend/CardDrawer.tsx
+++ frontend/CardDrawer.tsx
@@ -1,16 +1,18 @@
 import { CARD_FORMAT, ICard } from "../common.typings";
 
 export default function CardDrawer(props: { card: ICard }) {
-  let height = -10;
-  if (props.card.format === CARD_FORMAT.HEARTS) height = -234;
-  if (props.card.format === CARD_FORMAT.DIAMONDS) height = -458;
-  if (props.card.format === CARD_FORMAT.CLUBS) height = -681;
+  let height = -5;
+  if (props.card.format === CARD_FORMAT.HEARTS) height = -156;
+  if (props.card.format === CARD_FORMAT.DIAMONDS) height = -306;
+  if (props.card.format === CARD_FORMAT.CLUBS) height = -456;
 
   return (
     <div
       className="card-drawer"
+      data-number={props.card.number}
+      data-format={props.card.format}
       style={{
-        backgroundPositionX: -7 + (props.card.number - 1) * -153,
+        backgroundPositionX: -7 + (props.card.number - 1) * -103,
         backgroundPositionY: height,
       }}
     />
