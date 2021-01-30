f995db2ae8847de5406f4ffa9441ee90a3eb3d55
aghArdeshir <ardeshireo@gmail.com>
2021-01-31 00:12:18 +0330

    add auto picking for mustPick&RefuseCard

--- frontend/PickingPage.tsx
+++ frontend/PickingPage.tsx
@@ -1,11 +1,56 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { GAME_ACTION } from "../common.typings";
 import Card from "./Card";
 import { GameStateContext } from "./GameStateContext";
 import { socketService } from "./socket-service";
 
+const AUTO_SELECT_TIEMOUT = 1;
+
 export default function PickingPage() {
   const gameState = useContext(GameStateContext);
+  const [countdown, setCountdown] = useState(AUTO_SELECT_TIEMOUT);
+  const intervalRef = useRef<NodeJS.Timeout>(null);
+
+  useEffect(() => {
+    if (
+      gameState.nextAction === GAME_ACTION.PICK_CARDS &&
+      gameState.player.isTurn
+    ) {
+      if (gameState.mustPickCard || gameState.mustRefuseCard) {
+        intervalRef.current = setInterval(() => {
+          console.log("OH SHIT");
+          setCountdown((c) => {
+            if (c > 0) {
+              return c - 0.1;
+            } else {
+              clearInterval(intervalRef.current);
+              setCountdown(AUTO_SELECT_TIEMOUT);
+            }
+          });
+        }, 100);
+      }
+    } else {
+      clearInterval(intervalRef.current);
+      setCountdown(AUTO_SELECT_TIEMOUT);
+    }
+  }, [gameState]);
+
+  useEffect(() => {
+    if (countdown <= 0) {
+      if (
+        gameState.nextAction === GAME_ACTION.PICK_CARDS &&
+        gameState.player.isTurn
+      ) {
+        clearInterval(intervalRef.current);
+        setCountdown(AUTO_SELECT_TIEMOUT);
+        if (gameState.mustPickCard) {
+          socketService.pickCard();
+        } else if (gameState.mustRefuseCard) {
+          socketService.refuseCard();
+        }
+      }
+    }
+  }, [gameState, countdown]);
 
   if (
     gameState.nextAction !== GAME_ACTION.PICK_CARDS ||
@@ -41,14 +86,16 @@ export default function PickingPage() {
         className="accept-card action-button"
         onClick={() => socketService.pickCard()}
       >
-        👍 Pick
+        👍 Pick{" "}
+        {countdown && gameState.mustPickCard ? countdown.toFixed(1) : ""}
       </button>
       <button
         disabled={gameState.mustPickCard}
         className="refuse-card action-button"
         onClick={() => socketService.refuseCard()}
       >
-        👎 Pass
+        👎 Pass{" "}
+        {countdown && gameState.mustRefuseCard ? countdown.toFixed(1) : ""}
       </button>
     </>
   );
